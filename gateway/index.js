79b7a5bff0b9b7b23fb098544d31f376559b0400
Abin Babu <abinbabuonline@gmail.com>
2024-11-18 22:57:10 +0530

    API Gateway

--- gateway/index.js
+++ gateway/index.js
@@ -15,9 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/' , proxy('http://localhost:3000/')) //auth service
+app.use('/auth' , proxy('http://localhost:3000/')) //auth service
 app.use('/post' , proxy('http://localhost:3001/'))
-app.use('/comment' , proxy('http://localhost:3002/'))
+app.use('/comment', proxy('http://localhost:3002/'));
 
 app.listen(8000 , ()=> {
     console.log('Running on 8000')
