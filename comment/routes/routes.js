28ced3e7d2ab97d8d130a76f0e6d20e0f6c8be29
Abin Babu <abinbabuonline@gmail.com>
2024-11-18 22:57:41 +0530

    comment authenticated

--- comment/routes/routes.js
+++ comment/routes/routes.js
@@ -3,8 +3,11 @@ const express=require('express')
 const {addComment,getComment} =require('../controller/commentController.js')
 const router=express.Router()
 
-router.get('/api/commentservice/getcomment',getComment)
-router.post('/api/commentservice/comment',addComment)
+const authenticate = require('../../auth/middleware/authMiddleware.js')
+
+router.get('/api/commentservice/getcomment' , authenticate , getComment)
+router.post('/api/commentservice/comment', addComment)
+
 module.exports=router
 
 
