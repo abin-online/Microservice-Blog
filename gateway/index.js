d5514cf64a5d7e0e013400292bab33badf3cd631
Abin Babu <abinbabuonline@gmail.com>
2024-11-19 16:10:36 +0530

    solved the gateway bug

--- gateway/index.js
+++ gateway/index.js
@@ -2,21 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const proxy = require('express-http-proxy');
 
-// const startPostServer = require('../post');
-// const startAuthServer = require('../auth/app');
-// const startCommentServer = require('../comment');
-
 const app = express();
 
-// startAuthServer()
-// startPostServer()
-// startCommentServer()
+
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/auth' , proxy('http://localhost:3000/')) //auth service
 app.use('/post' , proxy('http://localhost:3001/'))
+app.use('/auth' , proxy('http://localhost:3000/')) //auth service
 app.use('/comment', proxy('http://localhost:3002/'));
 
 app.listen(8000 , ()=> {
